24e5272493ca789599e996130fb1ab0c79ef04e2
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 16:31:10 +0530

    adding one unit test case

---
+++ continuous_range_test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 16:12:57 2023
+
+@author: PGS2KOR
+"""
+
+from get_continuous_range import get_continuous_range
+
+assert get_continuous_range([4,5]) == "Range, Readings\n4-5, 2"
---
+++ get_continuous_range.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 16:27:57 2023
+
+@author: PGS2KOR
+"""
+
+def get_continuous_range(samples):
+    return
