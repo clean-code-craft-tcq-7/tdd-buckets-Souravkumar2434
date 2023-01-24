86d7c86fad18a688f5083f18feb86cd362f7393c
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 22:03:16 +0530

    adding test case for checking the continuity and get_the_difference_between_consecutive_samples

--- get_continuous_range.py
+++ get_continuous_range.py
@@ -5,18 +5,15 @@ Created on Tue Jan 24 16:27:57 2023
 @author: PGS2KOR
 """
 
-def check_status_of_continuity_or_repeatidity_of_data(firstvalue, secondvalue):
-    if(secondvalue- firstvalue == 1)\
-        or (secondvalue- firstvalue == 0):
-            return True
+def check_status_of_continuity_or_repeatidity_of_data(value):
+    if(value <= 1):
+        return True
     return False
 
-def get_continuous_range(samples):
-    start_value = samples[0]
-    next_value = sample[0]
-    
-    for sample in samples:
-        if(check_status_of_continuity_or_repeatidity_of_data):
-            next_value = sample
-            continue
-    return
+def get_difference_between_the_consecutive_samples(samples, diff_of_samples):
+    for i in range(len(samples) - 1):
+        diff_of_samples.append(samples[i +1] - samples[i])
+    diff_of_samples.insert(0,0)   #to make length equal to samples
+    return diff_of_samples
+
+
