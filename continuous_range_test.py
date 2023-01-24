f857568f0ca98bac6a67f9d5d5f0a1f3540bcf84
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 22:20:47 +0530

    adding test case and functionality to get continuous range and number of readings between them

--- continuous_range_test.py
+++ continuous_range_test.py
@@ -5,13 +5,13 @@ Created on Tue Jan 24 16:12:57 2023
 @author: PGS2KOR
 """
 from check_validity_of_input_data import check_validity_of_input_data
-from get_continuous_range import get_continuous_range,\
+from get_continuous_range import get_continuous_range_and_number_of_readings,\
     check_status_of_continuity_or_repeatidity_of_data,\
         get_difference_between_the_consecutive_samples
- 
+
 
 assert check_validity_of_input_data([4,5]) == "<class 'list'>"
-assert get_difference_between_the_consecutive_samples([4,5]) == [0,1]
-assert check_status_of_continuity_or_repeatidity_of_data([4,5]) == True
-assert get_continuous_range([4,5]) == "Range, Readings\n4-5, 2"
+assert get_difference_between_the_consecutive_samples([4,5], []) == [0,1]
+assert check_status_of_continuity_or_repeatidity_of_data(4) == False
+assert get_continuous_range_and_number_of_readings([4,5]) == ([[4, 5]], [2])
 
--- get_continuous_range.py
+++ get_continuous_range.py
@@ -10,10 +10,28 @@ def check_status_of_continuity_or_repeatidity_of_data(value):
         return True
     return False
 
-def get_difference_between_the_consecutive_samples(samples, diff_of_samples):
+def get_difference_between_the_consecutive_samples(samples , diff_of_samples):
     for i in range(len(samples) - 1):
         diff_of_samples.append(samples[i +1] - samples[i])
-    diff_of_samples.insert(0,0)   #to make length equal to samples
+    diff_of_samples.insert(0 , 0)   #to make length equal to samples
     return diff_of_samples
 
+def get_continuous_range_and_number_of_readings(samples):
+    continuous_range = []
+    continuous_range_indexes = []
+    diff_of_samples = []
+    number_of_readings = []
+
+    diff_of_samples = get_difference_between_the_consecutive_samples(samples, diff_of_samples)
+    for value in range(len(diff_of_samples)):
+        if(check_status_of_continuity_or_repeatidity_of_data(diff_of_samples[value])):
+            continuous_range_indexes.append(value)
+        else:
+            continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
+            number_of_readings.append(len(continuous_range_indexes))
+            continuous_range_indexes = []
+            continuous_range_indexes.append(value)
+    continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
+    number_of_readings.append(len(continuous_range_indexes))
+    return (continuous_range, number_of_readings)
 
