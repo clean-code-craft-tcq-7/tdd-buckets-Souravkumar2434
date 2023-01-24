4552946b42ca50abb1d1b2957d23c52080bc3634
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 19:12:06 +0530

    updating test case to check continuity status of a data fuctionality required in get_continuous_range

--- continuous_range_test.py
+++ continuous_range_test.py
@@ -5,8 +5,9 @@ Created on Tue Jan 24 16:12:57 2023
 @author: PGS2KOR
 """
 from check_validity_of_input_data import check_validity_of_input_data
-
+from get_continuous_range import get_continuous_range
 
 assert check_validity_of_input_data([4,5]) == "<class 'list'>"
 assert get_continuous_range([4,5]) == "Range, Readings\n4-5, 2"
+assert check_status_of_continuity_or_repeatidity_of_data([4,5]) == True
 
