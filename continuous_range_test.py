b0673806a1529a724e82d8f47943c02bd6ad224e
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 18:51:16 +0530

    changing the continuous_range_test file as numpy import not possibe

--- continuous_range_test.py
+++ continuous_range_test.py
@@ -4,9 +4,8 @@ Created on Tue Jan 24 16:12:57 2023
 
 @author: PGS2KOR
 """
-from check_validity_of_input_data import *
+from check_validity_of_input_data import check_validity_of_input_data
 
-import numpy as np
 
 assert check_validity_of_input_data([4,5]) == "<class 'list'>"
-assert check_validity_of_input_data(np.array([4,5])) == "<class 'numpy.ndarray'>"
+
