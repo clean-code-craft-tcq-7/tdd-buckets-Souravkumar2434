a39fb67c246383048166c7c95abf70e9daa1b3a7
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 22:24:44 +0530

    adding test_cases for else conditions also

--- continuous_range_test.py
+++ continuous_range_test.py
@@ -10,8 +10,10 @@ from get_continuous_range import get_continuous_range_and_number_of_readings,\
         get_difference_between_the_consecutive_samples
 
 
-assert check_validity_of_input_data([4,5]) == "<class 'list'>"
-assert get_difference_between_the_consecutive_samples([4,5], []) == [0,1]
+assert check_validity_of_input_data([4, 5]) == "<class 'list'>"
+assert get_difference_between_the_consecutive_samples([4, 5], []) == [0, 1]
 assert check_status_of_continuity_or_repeatidity_of_data(4) == False
-assert get_continuous_range_and_number_of_readings([4,5]) == ([[4, 5]], [2])
+assert check_status_of_continuity_or_repeatidity_of_data(1) == True
+assert get_continuous_range_and_number_of_readings([4, 5]) == ([[4, 5]], [2])
+assert get_continuous_range_and_number_of_readings([4, 5, 7, 8, 9]) == ([[4, 5], [7, 9]], [2, 3])
 
