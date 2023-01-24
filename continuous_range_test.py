a034e35d39acd98bd7a9fe33a789bf9981e8d5a0
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-24 18:54:36 +0530

    adding test case for getting continuous range- expectation fail

--- continuous_range_test.py
+++ continuous_range_test.py
@@ -8,4 +8,5 @@ from check_validity_of_input_data import check_validity_of_input_data
 
 
 assert check_validity_of_input_data([4,5]) == "<class 'list'>"
+assert get_continuous_range([4,5]) == "Range, Readings\n4-5, 2"
 
