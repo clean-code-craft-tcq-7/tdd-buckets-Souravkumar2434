f4822a907d00bb526c5f2789bf6b0d3199c2fd7b
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-30 18:20:32 +0530

    refactoring get_continuous_range_and_number_of_readings for limit complexity

--- get_continuous_range.py
+++ get_continuous_range.py
@@ -22,24 +22,28 @@ def get_difference_between_the_consecutive_samples(samples , diff_of_samples):
     diff_of_samples.insert(0 , 0)   #to make length equal to samples
     return diff_of_samples
 
-def get_continuous_range_and_number_of_readings(samples):
-    continuous_range = []
+def create_continuous_range_and_number_of_readings(continuous_range, number_of_readings, samples):
     continuous_range_indexes = []
     diff_of_samples = []
+    sorted_samples = get_sorted_array(samples)
+    diff_of_samples = get_difference_between_the_consecutive_samples(sorted_samples, diff_of_samples)
+    for value in range(len(diff_of_samples)):
+        if(check_status_of_continuity_or_repeatidity_of_data(diff_of_samples[value])):
+            continuous_range_indexes.append(value)
+        else:
+            continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
+            number_of_readings.append(len(continuous_range_indexes))
+            continuous_range_indexes = []
+            continuous_range_indexes.append(value)
+    continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
+    number_of_readings.append(len(continuous_range_indexes))
+    create_csv_format_string(continuous_range, number_of_readings)
+    return (continuous_range, number_of_readings)
+
+def get_continuous_range_and_number_of_readings(samples):
+    continuous_range = []
     number_of_readings = []
     if (check_input_data_is_valid(samples)):
-        sorted_samples = get_sorted_array(samples)
-        diff_of_samples = get_difference_between_the_consecutive_samples(sorted_samples, diff_of_samples)
-        for value in range(len(diff_of_samples)):
-            if(check_status_of_continuity_or_repeatidity_of_data(diff_of_samples[value])):
-                continuous_range_indexes.append(value)
-            else:
-                continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
-                number_of_readings.append(len(continuous_range_indexes))
-                continuous_range_indexes = []
-                continuous_range_indexes.append(value)
-        continuous_range.append([samples[continuous_range_indexes[0]], samples[continuous_range_indexes[-1]]])
-        number_of_readings.append(len(continuous_range_indexes))
-        create_csv_format_string(continuous_range, number_of_readings)
+        continuous_range, number_of_readings = create_continuous_range_and_number_of_readings(continuous_range, number_of_readings, samples)
     return (continuous_range, number_of_readings)
 
