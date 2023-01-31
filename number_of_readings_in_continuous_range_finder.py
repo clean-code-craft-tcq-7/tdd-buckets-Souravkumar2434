05454b696e8053a8ed23be1dcf48851277e41373
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-31 14:40:22 +0530

    adding functionality of finding number of readings in a continuous range for test cases to pass

--- number_of_readings_in_continuous_range_finder.py
+++ number_of_readings_in_continuous_range_finder.py
@@ -6,4 +6,7 @@ Created on Tue Jan 31 14:31:51 2023
 """
 
 def get_number_of_readings_in_continuous_range(continuousrange):
-    return 
+    number_of_readings = []
+    for value in continuousrange:
+        number_of_readings.append(value[1] - value[0] + 1)
+    return number_of_readings
