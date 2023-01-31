c12245be811665c177dc39df372936ed04a699dd
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-31 12:51:56 +0530

    adding test case for validating input data

---
+++ continuous_range_and number_of_readings_finder_test.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 12:47:12 2023
+
+@author: PGS2KOR
+"""
---
+++ validate_input_data.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 12:46:06 2023
+
+@author: PGS2KOR
+"""
+
+def check_input_data_is_ok(input_samples):
+    return
