19c2acebf6dad0aed05383092219cd0e34b89a9e
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-31 13:10:18 +0530

    adding test cases for continuous range finder

--- continuous_range_and_number_of_readings_finder_test.py
+++ continuous_range_and_number_of_readings_finder_test.py
@@ -1,4 +1,20 @@
-from validate_input_data import check_input_data_is_ok
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 12:47:12 2023
 
+@author: PGS2KOR
+"""
+from validate_input_data import check_input_data_is_ok
+from continuous_range_finder import get_continuous_range
+
+# check validity of data
 assert check_input_data_is_ok([]) == True
 assert check_input_data_is_ok('') == False
+
+#continuous range test
+assert get_continuous_range([]) == []
+assert get_continuous_range([1]) == []
+assert get_continuous_range([4, 5]) == [[4 - 5]]
+assert get_continuous_range([4, 5, 6]) == [[4 - 6]]
+assert get_continuous_range([4, 6, 5, 7]) == [[4 - 7]]
+assert get_continuous_range([4, 5, 6, 9, 8, 10]) == [[4 - 6], [8 - 10]]
---
+++ continuous_range_finder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 13:02:28 2023
+
+@author: PGS2KOR
+"""
+
+def get_continuous_range(input_samples):
+    return
