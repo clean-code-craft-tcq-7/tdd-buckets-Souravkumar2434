ddcbcbf0d2f0791c2983d24d406e2cde156cccb9
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-31 14:39:21 +0530

    adding test cases for finding number of readings in a continuous range functionality

--- continuous_range_and_number_of_readings_finder_test.py
+++ continuous_range_and_number_of_readings_finder_test.py
@@ -9,6 +9,7 @@ from continuous_range_finder import get_continuous_range,\
     get_sorted_array, check_status_of_continuity_or_repeatidity_of_data,\
         get_difference_between_the_consecutive_samples,\
             calculate_continuous_range
+from number_of_readings_in_continuous_range_finder import get_number_of_readings_in_continuous_range
 
 # check validity of data
 assert check_input_data_is_ok([]) == True
@@ -39,3 +40,11 @@ assert calculate_continuous_range([4, 5], []) == [[4, 5]]
 assert calculate_continuous_range([4, 5, 6], []) == [[4, 6]]
 assert calculate_continuous_range([4, 6, 5, 7], []) == [[4, 7]]
 assert calculate_continuous_range([4, 5, 6, 9, 8, 10], []) == [[4, 6], [8, 10]]
+
+
+#number of readings test
+assert get_number_of_readings_in_continuous_range([]) == []
+assert get_number_of_readings_in_continuous_range([[4, 5]]) == [2]
+assert get_number_of_readings_in_continuous_range([[4, 6]]) == [3]
+assert get_number_of_readings_in_continuous_range([[4, 7]]) == [4]
+assert get_number_of_readings_in_continuous_range([[4, 6] , [8, 10]]) == [3, 3]
---
+++ number_of_readings_in_continuous_range_finder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 14:31:51 2023
+
+@author: PGS2KOR
+"""
+
+def get_number_of_readings_in_continuous_range(continuousrange):
+    return 
