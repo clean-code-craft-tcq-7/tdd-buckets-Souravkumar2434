959bc4d79c0689d5436ac5972e5591e41df02e92
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-01-31 16:33:57 +0530

    updating function name based on functionality

--- continuous_range_and_number_of_readings_finder.py
+++ continuous_range_and_number_of_readings_finder.py
@@ -8,12 +8,14 @@ from validate_input_data import check_input_data_is_ok
 from continuous_range_finder import get_continuous_range
 from number_of_readings_in_continuous_range_finder\
     import get_number_of_readings_in_continuous_range
+from output_in_csv_format import create_csv_format_string
 
 
-def get_continuous_range_and_number_of_readings(samples):
+def get_continuous_range_and_number_of_readings_and_generate_output(samples):
     continuous_range = []
     number_of_readings = []
     if (check_input_data_is_ok(samples)):
         continuous_range = get_continuous_range(samples)
         number_of_readings = get_number_of_readings_in_continuous_range(continuous_range)
+        create_csv_format_string(continuous_range, number_of_readings)
     return (continuous_range, number_of_readings)
--- continuous_range_and_number_of_readings_finder_test.py
+++ continuous_range_and_number_of_readings_finder_test.py
@@ -12,7 +12,7 @@ from continuous_range_finder import get_continuous_range,\
 from number_of_readings_in_continuous_range_finder\
     import get_number_of_readings_in_continuous_range
 from continuous_range_and_number_of_readings_finder\
-    import get_continuous_range_and_number_of_readings
+    import get_continuous_range_and_number_of_readings_and_generate_output
 from output_in_csv_format import create_csv_format_string
 
 # check validity of data
@@ -53,15 +53,17 @@ assert get_number_of_readings_in_continuous_range([[4, 6]]) == [3]
 assert get_number_of_readings_in_continuous_range([[4, 7]]) == [4]
 assert get_number_of_readings_in_continuous_range([[4, 6] , [8, 10]]) == [3, 3]
 
-#continuous_range_and_number_of_readings test
-assert get_continuous_range_and_number_of_readings([]) == ([],[])
-assert get_continuous_range_and_number_of_readings('') == ([],[])
-assert get_continuous_range_and_number_of_readings([1]) == ([],[])
-assert get_continuous_range_and_number_of_readings([4, 5]) == ([[4, 5]],[2])
-assert get_continuous_range_and_number_of_readings([4, 5, 6]) == ([[4, 6]],[3])
-assert get_continuous_range_and_number_of_readings([4, 6, 5, 7]) == ([[4, 7]],[4])
-assert get_continuous_range_and_number_of_readings([4, 5, 6, 9, 8, 10]) == ([[4, 6], [8, 10]],[3, 3])
-
 #creating output in csv format test
 assert create_csv_format_string([[4, 5]], [2]) == "Range, Readings\n4, 5, 2"
 assert create_csv_format_string([[4, 5], [7, 8]], [2, 2]) == "Range, Readings\n4, 5, 2\n7, 8, 2"
+
+#continuous_range_and_number_of_readings test
+assert get_continuous_range_and_number_of_readings_and_generate_output([]) == ([],[])
+assert get_continuous_range_and_number_of_readings_and_generate_output('') == ([],[])
+assert get_continuous_range_and_number_of_readings_and_generate_output([1]) == ([],[])
+assert get_continuous_range_and_number_of_readings_and_generate_output([4, 5]) == ([[4, 5]],[2])
+assert get_continuous_range_and_number_of_readings_and_generate_output([4, 5, 6]) == ([[4, 6]],[3])
+assert get_continuous_range_and_number_of_readings_and_generate_output([4, 6, 5, 7]) == ([[4, 7]],[4])
+assert get_continuous_range_and_number_of_readings_and_generate_output([4, 5, 6, 9, 8, 10]) == ([[4, 6], [8, 10]],[3, 3])
+
+
