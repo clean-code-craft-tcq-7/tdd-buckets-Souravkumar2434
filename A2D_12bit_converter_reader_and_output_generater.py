4de7557dd62266a168f7e31fd27e3e1301e26643
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-02-06 15:15:21 +0530

    adding functionality of converting 12bit value into current amps

--- A2D_12bit_converter_reader_and_output_generater.py
+++ A2D_12bit_converter_reader_and_output_generater.py
@@ -5,5 +5,10 @@ Created on Mon Feb  6 14:39:53 2023
 @author: PGS2KOR
 """
 
+def convert_12bit_A2D_value_to_current_amps(value):
+    current_value = round((10 * value) / 4094)
+    return current_value
+
+
 def read_12bit_A2D_converter_and_generate_output(samples):
     return
