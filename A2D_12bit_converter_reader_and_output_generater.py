354edfa0f0e1df7f2ae313eff151fe6378c326d2
APAC\PGS2KOR <souravkumar.panghal@in.bosch.com>
2023-02-06 17:25:25 +0530

    updating Kata.txt with latest requirements

--- A2D_12bit_converter_reader_and_output_generater.py
+++ A2D_12bit_converter_reader_and_output_generater.py
@@ -5,7 +5,8 @@ Created on Mon Feb  6 14:39:53 2023
 @author: PGS2KOR
 """
 from Limits import A2D_LIMITS
-from continuous_range_and_number_of_readings_finder import get_continuous_range_and_number_of_readings_and_generate_output
+from continuous_range_and_number_of_readings_finder import\
+    get_continuous_range_and_number_of_readings_and_generate_output
 
 def convert_12bit_A2D_value_to_current_amps(value):
     current_value = round((10 * value) / 4094)
@@ -22,5 +23,6 @@ def read_12bit_A2D_converter_and_generate_output(samples):
             current_samples.append(convert_12bit_A2D_value_to_current_amps(value))
         else:
             print("An error in the values of A2D")
-    continuous_range, number_of_readings = get_continuous_range_and_number_of_readings_and_generate_output(current_samples)
+    continuous_range, number_of_readings = \
+        get_continuous_range_and_number_of_readings_and_generate_output(current_samples)
     return (continuous_range, number_of_readings)
